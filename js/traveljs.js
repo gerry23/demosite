3e309f85c4c9df83c3ee9949d621f9e0a9bc0e3e
gerry23 <no985432938@gmail.com>
2017-05-30 03:28:36 +0800

    travel

---
+++ js/traveljs.js
@@ -0,0 +1,46 @@
+
+$(document).ready(function(){
+
+    $(".button-collapse").sideNav();
+
+    $('.parallax').parallax();
+
+    $('.slider').slider({
+          height:700,
+     });
+ });      
+
+ $("a.changeVolume").on("click",function(){
+  var status = $(this).attr("rel");
+
+    if(status == "closeVolume")
+        {
+          $("video")[0].muted = false;
+          $(this).attr("rel","openVolume");
+          $(this).find("i.material-icons").text("volume_up");
+        }
+    else
+        {
+          $("video")[0].muted = true;
+          $(this).attr("rel","closeVolume");
+          $(this).find("i.material-icons").text("volume_off");
+        }
+          });
+
+$("ul li a").on("click",function(){
+  var a_elm = $(this);
+  var target_id = a_elm.attr("href");
+  var topTo = 0 ;
+   if(target_id != "#"){
+     topTo = $(target_id).position().top;
+   }
+
+  $("html,body").animate({
+    scrollTop: topTo
+  },600,function()
+  {
+  $("ul li[ class ='active']").removeClass('active');
+    a_elm.parent().addClass('active');
+  });
+  return false;
+});
